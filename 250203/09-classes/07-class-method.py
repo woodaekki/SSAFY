522840f2afca6b052d36fa2c4f0ea5a76d0434ed
Kim Seon Woo <dora01622@naver.com>
2025-02-03 10:58:59 +0900

    Add files via upload

---
+++ 250203/09-classes/07-class-method.py
@@ -0,0 +1,14 @@
+class Person:
+    population = 0
+
+    def __init__(self, name):
+        self.name = name
+        Person.increase_population()
+    
+    @classmethod # 클래스 메서드, 첫번째 인자로 메서드 호출
+    def increase_population(cls):
+        cls.population += 1
+
+person1 = Person('Alice')
+person2 = Person('Bob')
+print(Person.population)  # 2
