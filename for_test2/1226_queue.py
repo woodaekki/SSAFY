d1682426f704f4c54d50adbc7fa66b7bf8b90599
kimseonwoo <dora01622@naver.com>
2025-02-28 17:58:40 +0900

    202502285555

--- for_test2/1226_queue.py
+++ for_test2/1226_queue.py
@@ -14,28 +14,25 @@ def bfs(x, y):
     while queue:
         x, y = queue.popleft()
         for di, dj in directions:
-            ni, nj = x+di, y+dj
+            ni, nj = x + di, y + dj
             # 경계 내에 있고 벽이 아닌데 방문하지 않은 경우
-            if 0<=ni<16 and 0<=nj<16 and arr[ni][nj]!=1 and visited[ni][nj]:
+            if 0 <= ni < 16 and 0 <= nj < 16 and arr[ni][nj] != 1 and visited[ni][nj] == 0:
                 queue.append((ni, nj))
                 visited[ni][nj] = 1
                 # 도착점에 다다르면 1반환
-                if (ni,nj) == end:
+                if (ni, nj) == end:
                     return 1
     return 0
 
 T = 10
 for t in range(1, T+1):
-    tc = int(input())
-    arr = [list(map(int, input().split())) for _ in range(16)]
-    visited = [[0]*16 for _ in range(16)] # 방문목록 생성 
-    start_i, start_j = 1, 1
-    end = (13, 13)
+    tc = int(input())  # 테스트 케이스 번호 (사용 안 함)
+    arr = [list(map(int, input().split())) for _ in range(16)]  # 16x16 지도 입력
+    visited = [[0]*16 for _ in range(16)]  # 방문목록 생성
+    start = (1,1)  # 출발점 (1,1) 위치
+    end = (13,13)  # 도착점 (13,13) 위치
 
-    # 출발점 찾기
-    for i in range(16):
-        for j in range(16):
-            if arr[i][j] == 2:
-                result = bfs(i, j)
-    print(f'#{t} {result}')
-
+    # 출발점이 2인 경우에만 시작
+    if start == 2:
+        result = bfs(start)  # bfs 함수 호출
+        print(f'#{t} {result}')
