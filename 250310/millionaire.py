e80380fa7dd8f9098ac0754895d2250596ed2aaf
kimseonwoo <dora01622@naver.com>
2025-03-10 17:51:32 +0900

    250310

---
+++ 250310/millionaire.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("1.txt", "r")
+
+def millionaire():
+    # 최댓값 구하기
+    max_arr = arr[0]
+    for i in range(n):
+        max_arr = max(max_arr, arr[i])
+
+
+
+
+T = int(input())
+for t in range(1, T+1):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    print(f'{t} {millionaire()}')
