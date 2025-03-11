b3699ff3ee565631243f447f6cbb3704e53c3605
김선우 <dora01622@naver.com>
2025-03-11 23:46:13 +0900

    250311_2

--- 250310/millionaire.py
+++ 250310/millionaire.py
@@ -1,8 +1,33 @@
 import sys
 sys.stdin = open("1.txt", "r")
 
+"""
+1. 최대값 찾아서 
+2. 그 이전 가격까지 매수해서, 최댓값에서 매수
+3. 최댓값까지 삭제하고, 그 다음 인덱스부터 다시 최댓값 찾아서 다시 매매 반복
+4. 가격 리스트 빌 때까지
+"""
+from collections import deque
+
 def millionaire():
-    
+    margin = 0
+    prices_deque = deque(prices)  
+    while len(prices_deque) > 0:
+        # 최댓값 찾아서 
+        max_v = max(prices_deque)
+        max_idx = prices_deque.index(max_v)
+
+        # 최댓값 이전까지 사기 
+        buy = sum(list(prices_deque)[:max_idx])
+        sell = max_v * max_idx
+        margin += sell - buy
+
+        for _ in range(max_idx + 1): # 최댓값 인덱스까지 삭제, 그 다음 인덱스부터 다시 최댓값 찾고.. 반복 
+            if len(prices_deque) > 0:
+                prices_deque.popleft()  
+            else:
+                break
+    return margin
 
 T = int(input())
 for t in range(1, T + 1):
---
+++ 250312/bus.py
@@ -0,0 +1,37 @@
+def bus(type, a, b, visited):
+    # 무조건 a, b는 방문해야 함
+    visited[a] += 1
+    visited[b] += 1
+
+    if type == 1: 
+        for i in range(a+1, b):
+            visited[i] += 1
+            
+    elif type == 2: 
+        if a % 2 == 0:
+            for i in range(a+2, b, 2):
+                visited[i] += 1
+        else:
+            for i in range(a+2, b, 2):
+                visited[i] += 1
+
+    elif type == 3:  
+        if a % 2 == 0:
+            for i in range(a+1, b):
+                if i % 4 == 0:
+                    visited[i] += 1
+        else:
+            for i in range(a+1, b):
+                if i % 3 == 0 and i % 10 != 0:
+                    visited[i] += 1
+    
+    return max(visited)
+
+T = int(input())
+for t in range(1, T + 1):
+    n = int(input())
+    visited = [0] * 1001
+    for _ in range(n):
+        type, a, b = list(map(int, input().split()))
+        bus(type, a, b, visited)
+    print(f'#{t} {max(visited)}')
