9f8a4216433a1d01b6585cfdde3c3ff09f9ccd1c
김선우 <dora01622@naver.com>
2025-03-12 21:59:38 +0900

    250312

--- 250312/prize.py
+++ 250312/prize.py
@@ -1,31 +1,32 @@
 import sys
 sys.stdin = open("7.txt", "r")
 
-def change(k):
-    global result
-    n = len(cards)
-
-    # numbers = int("".join(cards))
-    # k번째 depth 에 이미 나온 수이면 반환
-    # 아니면 k번째에 잘 저장
-
-    if k == change_cnt:
-        numbers = int("".join(cards))
-        result = max(result, numbers)
-        return
-
-    for i in range(n-1):
-        for j in range(i+1, n):
-            # 데이터 교환
-            cards[i], cards[j] = cards[j], cards[i]
-            change(k+1)
-            # 원복
-            cards[i], cards[j] = cards[j], cards[i]
-
-# 백트래킹: 같은 깊이의 같은 수가 나오면 가지를 치자.
+def change(k, n):
+    global maxv
+    if k == n: # 모든 교환이 끝난 경우 
+        tmp = int("".join(card))
+        if maxv < tmp:
+            maxv = tmp
+    else:
+        for i in range(m-1): # 왼쪽 카드
+            for j in range(i+1, m): # 오른쪽 카드 
+                # (교환횟수, 만들어지는 값을 저장해서 중복 방지)
+                card[i], card[j] = card[j], card[i] # 카드 교환
+                # 가지 치기 
+                tmp = int("".join(card)) 
+                # 만들어진적이 없으면 
+                if tmp not in memo[k]:
+                    memo[k].append(tmp)
+                    change(k+1, n)
+                card[i], card[j] = card[j], card[i] # 원상복구 
 
 T = int(input())
 for t in range(1, T+1):
-    cards, change_cnt = input.split()
-    change_cnt = int(change_cnt)
-    cards = list(cards)
+    card, n = input().split()
+    card = list(card)
+    n = int(n)
+    m = len(card)
+    maxv = 0
+    memo = [[] for _ in range(10)] # 최대 교환횟수가 10
+    change(0, n)
+    print(f'#{t} {maxv}')
