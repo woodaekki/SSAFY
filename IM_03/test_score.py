23e7101cf876ede5c3d42dfb9d8dceff634ed6f0
김선우 <dora01622@naver.com>
2025-03-20 22:05:31 +0900

    250320

--- IM_03/test_score.py
+++ IM_03/test_score.py
@@ -1,11 +1,12 @@
 import sys
-sys.stdin = open("test.txt", "r")
+sys.stdin = open("test.txt", "r")  # 디버깅 시 주석 처리 가능
 
+def solve(n, arr):  # n과 arr을 매개변수로 추가
 
-def solve():
-    pass
 
 T = int(input())
 for t in range(1, T + 1):
     n = int(input())
-    arr = list(map(int, input().split()))  
+    arr = list(map(int, input().split()))
+    result = solve(n, arr)  # n과 arr을 전달
+    print(result)
