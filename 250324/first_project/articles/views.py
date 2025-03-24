08018aabc7b6365d69de8c864f3b5ceebc83b32c
kimseonwoo <dora01622@naver.com>
2025-03-24 11:48:00 +0900

    250324_2

---
+++ 250324/first_project/articles/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def list_article(request):
+    return render(request, "list_article.html")
