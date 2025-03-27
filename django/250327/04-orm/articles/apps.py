78c93ed22d25563cc4269f957ef2cf50e3f705d8
kimseonwoo <dora01622@naver.com>
2025-03-27 17:43:14 +0900

    25

--- django/250327/04-orm/articles/apps.py
+++ django/250327/04-orm/articles/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LibrariesConfig(AppConfig):
+class ArticlesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'libraries'
+    name = 'articles'
--- django/250327/04-orm/crud/urls.py
+++ django/250327/04-orm/crud/urls.py
@@ -1,7 +1,8 @@
-"""library_management URL Configuration
+"""
+URL configuration for crud project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -12,24 +13,11 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-# """
-# from django.contrib import admin
-# from django.urls import path
-# from libraries import views
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', views.index),
-#     path('recommend/', views.recommend),
-#     path('bestseller/', views.bestseller),
-# ]
-
+"""
 from django.contrib import admin
-from django.urls import path, include  # include를 추가
+from django.urls import path, include
 
-# 'libraries.urls'를 include로 추가하여 urls.py 파일을 모듈화합니다.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('libraries.urls')),  # libraries.urls를 포함시킴
+    path('articles/', include('articles.urls')),
 ]
-
