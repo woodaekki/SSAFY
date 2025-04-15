ad03cb04035faaf696bbef02bcb96e586e07ab32
정석원 <jsw3603@naver.com>
2025-04-15 09:04:31 +0900

    .

--- study/5.py
+++ study/5.py
@@ -3,11 +3,27 @@ sys.stdin = open("1.txt", "r")
 
 # 백준: 과일 탕후루
 def solve():
-    pass
+    front = 0
+    back = len(arr)-1
+    # 앞에거 뺐을때
+    arr.remove((arr[0:front]))
+    if len(set(arr)) == 2:
+        return len(arr)
+    else:
+        # 원복
+        arr.append(arr[front])
 
-T = int(input())
-for T in range(T):
-    arr = list(map(int, input().split()))
-    print(solve())
+    # 뒤에거 뺐을때
+    i = 0
+    arr.remove((arr[back:back-i:-1]))
+    if len(set(arr)) == 2:
+        return len(arr)
+    else:
+        # 원복
+        arr.append(arr[back])
+
+n = int(input())
+arr = list(map(int, input().split()))
+# print(solve())
 
 
---
+++ study/dslr.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open("2.txt", "r")
+
+def solve():
+
+
+
+T = int(input())
+for _ in range(T):
+    a, b = input().split()
+    a = list(a)
+
+
+
