fa46db8360c3ce4083fb30e0ce8e00a881c40cf2
kimseonwoo <dora01622@naver.com>
2025-04-18 12:20:34 +0900

    .

--- django/250326/03-model/articles/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# 게시글이 저장될 테이블을 설계하는 클래스
-class Article(models.Model):
-    title = models.CharField(max_length=10) # 제목 (제약조건: char은 길이 제한)
-    content = models.TextField() # 내용 (text는 제한 없음)
-    created_at = models.DateTimeField(auto_now_add=True) # 작성 시간 (처음 생성될때만 자동저장)
-    updated_at = models.DateTimeField(auto_now=True) # 수정 시간 (수정할때마다 자동저장)
--- django/250327/04-orm/articles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path  # 여기서 path를 임포트합니다.
-from . import views
-
-app_name = 'articles'
-urlpatterns = [
-    path('', views.index, name='index'),
-]
