41f36dd7ac16eec2b0f18a4fcfad93a080608980
kimseonwoo <dora01622@naver.com>
2025-05-12 17:53:39 +0900

    .

--- BAEKJOON/cut_the_trees.py
+++ BAEKJOON/cut_the_trees.py
@@ -1,2 +1,24 @@
+import sys
+sys.stdin = open("1.txt", "r")
+
+def solve():
+    cutting = arr[-1]
+    new_arr = []
+    for i in range(n):
+        new_arr.append(arr[i] - cutting)
+        sumv = 0
+        for j in range(n):
+            if new_arr[j] > 0:
+                sumv += new_arr[j]
+        if sumv > m:
+            return cutting
+        else:
+            cutting -= 1
+
+
+
 n, m = list(map(int, input().split())) # 나무의 수, 나무의 길이
-height = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+arr.sort()
+solve()
+# print(result)
