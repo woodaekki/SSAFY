79fbd3f468284f2fbc6d1acb47cea0ae6efae704
김선우 <dora01622@naver.com>
2025-05-12 20:47:59 +0900

    .

--- BAEKJOON/cut_the_trees.py
+++ BAEKJOON/cut_the_trees.py
@@ -2,23 +2,24 @@ import sys
 sys.stdin = open("1.txt", "r")
 
 def solve():
-    cutting = arr[-1]
-    new_arr = []
-    for i in range(n):
-        new_arr.append(arr[i] - cutting)
+    left, right = 0, arr[-1]
+    result = 0
+    while left <= right:
+        mid = (left + right) // 2
         sumv = 0
-        for j in range(n):
-            if new_arr[j] > 0:
-                sumv += new_arr[j]
-        if sumv > m:
-            return cutting
+        for i in range(n):
+            if arr[i] - mid > 0:
+                sumv += arr[i] - mid
+            else:
+                pass
+        if sumv >= m:
+            result = mid
+            left = mid + 1
         else:
-            cutting -= 1
+            right = mid - 1
+    return result
 
-
-
-n, m = list(map(int, input().split())) # 나무의 수, 나무의 길이
+n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 arr.sort()
-solve()
-# print(result)
+print(solve())
