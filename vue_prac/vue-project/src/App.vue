ed8b05de1dbc17f2a8d4052a63a92a7877003a3e
김선우 <dora01622@naver.com>
2025-05-19 20:39:40 +0900

    .

---
+++ vue_prac/vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script setup>
+import {ref} form 'vue'
+
+const count = ref(0)
+
+const increment = function() {
+  count.value++
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
